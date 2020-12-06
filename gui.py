0e995688cc3900aee5289ab61022570729a95333
Max Hauer <github@mhauer.de>
2020-12-06 14:25:35 +0100

    added tabs, frames, gris structure

--- gui.py
+++ gui.py
@@ -1,102 +1,144 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 
 # Create a list of checkboxes
 class Checkbar(tk.Frame):
-    def __init__(self, parent=None, picks=[], ):
+    def __init__(self, parent=None, picks=[] ):
         tk.Frame.__init__(self, parent)
         self.vars = []
-        for idx,pick in enumerate(picks):
+        for idx, pick in enumerate(picks):
             var = tk.IntVar()
             chk = tk.Checkbutton(self, text=pick, variable=var)
 
-            chk.grid(sticky="W", row=idx, column=0)
-    
-                
+            chk.pack(side="top",anchor="w")
+
             self.vars.append(var)
 
     def state(self):
         return map((lambda var: var.get()), self.vars)
 
 # Create a list of checkboxes
+
+"""
 class Entrybar(tk.Frame):
     def __init__(self, parent=None, picks=[], ):
         tk.Frame.__init__(self, parent)
         self.vars = []
-        for idx,pick in enumerate(picks):
-            
+        for idx, pick in enumerate(picks):
+
             Inputfield = tk.Entry(self, )
 
             chk.grid(sticky="W", row=idx, column=0)
-    
-                
+
             self.vars.append(var)
 
     def state(self):
         return map((lambda var: var.get()), self.vars)
+"""
 
-window = tk.Tk()
-window.title("Storygenerator")
-#window.geometry("500x500")
+root = tk.Tk()
+root.title("Storygenerator")
+root.minsize(width="250", height="430")
+root.maxsize(width="250", height="430")
+
+####################################### Definition ############################################
+
+#Define Tabs
+tab_parent = ttk.Notebook(root)
+t_main = tk.Frame(tab_parent)
+t_settings = tk.Frame(tab_parent)
+
+# Defines Frames
+#Entrygrid
+f_entry = tk.Frame(t_main, relief='ridge')
+f_entrylabels = tk.Frame(f_entry,relief="ridge")
+f_entryfields = tk.Frame(f_entry,relief="ridge")
+f_entrybutton = tk.Frame(f_entry,relief="ridge")
+#Componentgrid
+f_components = tk.Frame(t_main, relief='ridge')
+f_complabels = tk.Frame(f_components, relief='ridge')
+f_compcheck = tk.Frame(f_components, relief='ridge')
+#Issuegrid
+f_issues = tk.Frame(t_main, relief='ridge')
+f_issueslabels= tk.Frame(f_issues, relief='ridge')
+f_issuescheck= tk.Frame(f_issues, relief='ridge')
 
 # Define Label
-l_issueID = tk.Label( text="IssueID:")
-l_summary = tk.Label( text="Summary:")
-l_epiclink = tk.Label( text="Epic Link:")
-l_labels = tk.Label( text="Labels:")
-l_component = tk.Label( text="Component/s:")
-l_createissue = tk.Label( text="Create Issue:")
-l_reporter = tk.Label( text="Reporter:")
-l_assignee = tk.Label( text="Assignee:")
+l_issueID = tk.Label(f_entrylabels, text="IssueID:")
+l_summary = tk.Label(f_entrylabels, text="Summary:")
+l_epiclink = tk.Label(f_entrylabels, text="Epic Link:")
+l_labels = tk.Label(f_entrylabels, text="Labels:")
+l_component = tk.Label(f_complabels, text="Component/s:")
+l_createissue = tk.Label(f_issueslabels, text="Create Issue:")
+l_reporter = tk.Label(f_entrylabels, text="Reporter:")
+l_assignee = tk.Label(f_entrylabels, text="Assignee:")
 
 # Define Inpufield
-e_issueID = tk.Entry()
-e_summary = tk.Entry()
-e_epiclink = tk.Entry()
-e_labels = tk.Entry()
-e_reporter = tk.Entry()
-e_assignee = tk.Entry()
+e_issueID = tk.Entry(f_entryfields)
+e_summary = tk.Entry(f_entryfields)
+e_epiclink = tk.Entry(f_entryfields)
+e_labels = tk.Entry(f_entryfields)
+e_reporter = tk.Entry(f_entryfields)
+e_assignee = tk.Entry(f_entryfields)
 
-#e_fields = Entrybar(window,["A","B","C""D])
 
 # Define Button
-b_load = tk.Button( text='Load Issue')
-b_addLabel = tk.Button( text='Add Label')
-b_pushIssues = tk.Button( text='Generate Issues')
+b_load = tk.Button(f_entrybutton, text='Load Issue')
+b_addLabel = tk.Button(f_entrybutton,text='Add Label')
+b_generateIssues = tk.Button(t_main,text='Generate Issues')
 
 
 # Define Checkbox
-chk_columns = Checkbar(window, ["CMN","AMS","CF","SF"])
-chk_issues = Checkbar(window,["Analysis","Prep Business","Prep Testdata","Order"])
+chk_columns = Checkbar(f_compcheck, ["CMN", "AMS", "CF"])
+chk_issues = Checkbar(f_issuescheck, ["Analysis", "Prep Business", "Prep Testdata", "Order"])
 
 
-# Initialise
+####################################### Initialise ############################################
+
+#Tab
+tab_parent.add(t_main,text="Issuegenerator")
+tab_parent.add(t_settings,text="Settings")
+tab_parent.grid(row=0, column=0, sticky="nsew")
+
+# Frame
+f_entry.grid(row=1, column=0, sticky="nsew")
+f_entrylabels.grid(row=0, column=0,sticky="w")
+f_entryfields.grid(row=0, column=1,sticky="w")
+f_entrybutton.grid(row=0, column=2,sticky="wn")
+f_components.grid(row=2, column=0, sticky="nsew")
+f_complabels.grid(row=0, column=0, sticky="nsew")
+f_compcheck.grid(row=1, column=1,columnspan=2, sticky="nsew")
+f_issues.grid(row=3, column=0, sticky="nsew")
+f_issueslabels.grid(row=0, column=0, sticky="nsew")
+f_issuescheck.grid(row=1, column=1,columnspan=2, sticky="nsew")
+
 # Labels
-l_issueID.grid(row=0, column=0, padx=5, pady=5)
-l_summary.grid(row=1, column=0, padx=5, pady=5)
-l_epiclink.grid(row=2, column=0, padx=5, pady=5)
-l_labels.grid(row=3, column=0, padx=5, pady=5)
-l_component.grid(row=4, column=0, padx=5, pady=5)
-l_createissue.grid(row=6, column=0, padx=5, pady=5)
-l_reporter.grid(row=8, column=0, padx=5, pady=5)
-l_assignee.grid(row=9, column=0, padx=5, pady=5)
+l_issueID.pack()
+l_summary.pack()
+l_epiclink.pack()
+l_labels.pack()
+l_component.pack()
+l_createissue.pack()
+l_reporter.pack()
+l_assignee.pack()
 
 # Inpufields
-e_issueID.grid(row=0, column=1, padx=5, pady=5)
-e_summary.grid(row=1, column=1, padx=5, pady=5)
-e_epiclink.grid(row=2, column=1, padx=5, pady=5)
-e_labels.grid(row=3, column=1, padx=5, pady=5)
-e_reporter.grid(row=8, column=1, padx=5, pady=5)
-e_assignee.grid(row=9, column=1, padx=5, pady=5)
+e_issueID.pack()
+e_summary.pack()
+e_epiclink.pack()
+e_labels.pack()
+e_reporter.pack()
+e_assignee.pack()
 
 # Buttons
-b_load.grid(row=0, column=2, padx=5, pady=5)
-b_addLabel.grid(row=3, column=2, padx=5, pady=5)
-b_pushIssues.grid(row=9, column=0, columnspan=3, padx=5, pady=5)
+b_load.grid(row=0,column=0)
+b_addLabel.grid(row=5,column=0,pady=30)
+b_generateIssues.grid()
 
 # Checkbox
-chk_columns.grid(row=5,column=1)
-chk_issues.grid(row=7,column =1)
+chk_columns.pack()
+chk_issues.pack()
 
-# loop window
-window.mainloop()
+# loop root
+root.mainloop()
